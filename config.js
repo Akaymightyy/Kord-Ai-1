719abf1bcc114c547ac6fae34b292cfea4f67f35
Akaymightyy <163235792+Akaymightyy@users.noreply.github.com>
2026-01-24 07:50:50 +0100

    Update config.js
    Signed-off-by: Akaymightyy <163235792+Akaymightyy@users.noreply.github.com>

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || 'kord_ai-psk0BnXAzgoLepV9',
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-8zqQd8JxEaF0ZAFO',
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2349049541318",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "[,]",
